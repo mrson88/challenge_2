2ef6039e590fa5a3a360638b8736a262627b3363
MrSon <sonk9d@gmail.com>
2022-05-31 17:42:37 +0700

    lan 3

---
+++ lib/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const BackgoundColor = Color(0xFFF242A37);
+const HeaderCharacter = Color(0xFFFFFFFF);
+const chacrater_mes = TextStyle(fontSize: 17, color: Color(0xFFFFFFFF));
+const chacrater_name = TextStyle(fontSize: 11, color: Color(0xFFFFFFFF));
+const line_color = Color(0xFF000000);
+const hour_color = TextStyle(
+  fontSize: 13,
+  color: Color(0xFF4E586E),
+);
+const navi_color = Color(0xFF242A37);
---
+++ lib/data/full_data.dart
@@ -0,0 +1,137 @@
+List namePerson = [
+  {
+    'id': 1,
+    'name': 'Herman Pope',
+    'img':
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSU0XWsSi2UwpVRkeBTWrC9FvMh2gtFOB4S7w&usqp=CAU'
+  },
+  {
+    'id': 2,
+    'name': 'Sue Cadwell',
+    'img':
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRa-dYxWj241JrryDiuId58YpT6VeSijx0GZcIcOeF4Uoq6wuDps_XpYPDBP5lV5w9Wg40&usqp=CAU'
+  },
+  {
+    'id': 3,
+    'name': 'Ada Reyes',
+    'img':
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRV5acpdFV485URdOQrmBrtWfWKqSUVOE-mISWVzOjZ3WXvU8J56ThF6LgqiKHAx5zihSA&usqp=CAU'
+  },
+  {
+    'id': 4,
+    'name': 'Haley Dooman',
+    'img':
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfsDIVuPKLzize-H6WJyF8S7IOySmX6u0j7w&usqp=CAU'
+  },
+  {
+    'id': 5,
+    'name': 'Laura',
+    'img':
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJ0vCAMZl4F8X0F7QrdyErXzpKi25qDQfOsjFt9TuyR6bwWVHXQYduZAr_sO0gB9eyc9g&usqp=CAU'
+  },
+  {
+    'id': 6,
+    'name': 'Moldahj',
+    'img':
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMxF2-QCc3rZfHRh9Ga_mDQT4j4-mPCmbTcOKZM8HIOmXwF6-s1AfBzMshmXnW0bseTBE&usqp=CAU'
+  },
+  {
+    'id': 7,
+    'name': 'Yaghi',
+    'img':
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzTrV_3RsQtpYher2zj0BDNcNXj7Byj62Dqg&usqp=CAU'
+  }
+];
+
+List personMessages = [
+  {
+    "id": 1,
+    "name": "Herman Pope",
+    "img":
+        "https://drive.google.com/uc?export=view&id=1e32cUWbd4uqvesnuNIbZLzxk3tifdrR7",
+    "message": "Hey! How's it going?",
+    "created_at": "04:04AM",
+    "num_mess": "3",
+  },
+  {
+    "id": 2,
+    "name": "Sue Caldwell",
+    "img":
+        "https://drive.google.com/uc?export=view&id=1SXBr5a0Bo8_t2bxPhJ5A_YLaPnoRahuw",
+    "message": "What kind of music do you like?",
+    "created_at": "08:58PM",
+    "num_mess": "1",
+  },
+  {
+    "id": 3,
+    "name": "Ada Reyes",
+    "img":
+        "https://drive.google.com/uc?export=view&id=1Rk972v5pw4RibgZFv-KejLCLBwO2oUyn",
+    "message": "Sounds good to me!",
+    "created_at": "11:33PM",
+    "num_mess": "",
+  },
+  {
+    "id": 4,
+    "name": "Hallie Sandoval",
+    "img":
+        "https://drive.google.com/uc?export=view&id=1e0ECFl30yxCi-ktnmJHh2wac8tgeWMo9",
+    "message": "Hi Tina. How's your night going?",
+    "created_at": "06:58PM",
+    "num_mess": "",
+  },
+  {
+    "id": 5,
+    "name": "Dean Warren",
+    "img":
+        "https://img.freepik.com/free-photo/travel-technology-life-style-concept-young-bearded-photographer-taking-pictures-with-digital-camera_118342-23941.jpg?w=1800",
+    "message": "What did you do over the weekend?",
+    "created_at": "09:43PM",
+    "num_mess": "",
+  },
+  {
+    "id": 6,
+    "name": "Christopher",
+    "img":
+        "https://photo.wondershare.com/images/en/blog/photo-filter-download-4.jpg",
+    "message": "Hey! How's it going?",
+    "created_at": "04:04AM",
+    "num_mess": "",
+  },
+  {
+    "id": 7,
+    "name": "Reese",
+    "img":
+        "https://drive.google.com/uc?export=view&id=1GAUlWPxlxJ0UVZgM95_ieQ3tNsNheRPy",
+    "message": "What are you doing?",
+    "created_at": "03:45AM",
+    "num_mess": "",
+  },
+  {
+    "id": 8,
+    "name": "Jeffrey",
+    "img":
+        "https://c1.wallpaperflare.com/preview/997/210/523/woman-female-portrait-face.jpg",
+    "message": "Hey! How's it going?",
+    "created_at": "04:56AM",
+    "num_mess": "",
+  },
+  {
+    "id": 9,
+    "name": "Laura",
+    "img":
+        "https://enviragallery.com/wp-content/uploads/2019/08/Pre_Edit_1.jpg",
+    "message": "Hey! How's it going?",
+    "created_at": "07:04AM",
+    "num_mess": "",
+  },
+  {
+    "id": 10,
+    "name": "Lucy",
+    "img":
+        "https://gentlenobra.com/wp-content/uploads/2021/11/gai-xinh-khoe-dang.jpg",
+    "message": "Hey! How's it going?",
+    "created_at": "04:59AM",
+    "num_mess": "",
+  },
+];
