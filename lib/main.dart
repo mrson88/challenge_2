790d9c7c03c85868f3132df03b35773f4f20786d
MrSon <sonk9d@gmail.com>
2022-06-14 19:53:47 +0700

    push icon

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,16 @@ import 'package:challenge_2/constants.dart';
 import 'package:challenge_2/screens/home_page.dart';
 import 'package:flutter/material.dart';
 
-String asdas = '{"name":"Khdskjas","email":"hdajkdha"}';
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      color: backgroundColor,
       home: Scaffold(
         backgroundColor: backgroundColor,
         body: HomePage(),
